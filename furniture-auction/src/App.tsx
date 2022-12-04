93e4efbcc59de310c9a8492450d367b714fa5f3b
kyubhin <kyubhinhan@gmail.com>
2022-12-04 15:52:43 +0900

    Feat : eslint autofix apply
    eslint의 autofix 기능 적용함

--- furniture-auction/src/App.tsx
+++ furniture-auction/src/App.tsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
-import { atom, useAtom } from 'jotai'
-import './App.css'
+import { atom, useAtom } from 'jotai';
+import { getOriginalNode } from 'typescript';
+import './App.css';
 
 function App() {
-  const textAtom = atom('hello')
-const textLenAtom = atom((get) => get(textAtom).length)
-const uppercaseAtom = atom((get) => get(textAtom).toUpperCase())
+  const textAtom = atom('hello');
+  const textLenAtom = atom((get) => get(textAtom).length);
+  const uppercaseAtom = atom((get) => get(textAtom).toUpperCase());
 
-const Input = () => {
-  const [text, setText] = useAtom(textAtom)
-  return <input value={text} onChange={(e) => setText(e.target.value)} />
-}
+  const Input = () => {
+    const [text, setText] = useAtom(textAtom);
+    return <input value={text} onChange={(e) => setText(e.target.value)} />;
+  };
 
-const CharCount = () => {
-  const [len] = useAtom(textLenAtom)
-  return <div>Length: {len}</div>
-}
+  const CharCount = () => {
+    const [len] = useAtom(textLenAtom);
+    return <div>Length: {len}</div>;
+  };
 
-const Uppercase = () => {
-  const [uppercase] = useAtom(uppercaseAtom)
-  return <div>Uppercase: {uppercase}</div>
-}
+  const Uppercase = () => {
+    const [uppercase] = useAtom(uppercaseAtom);
+    return <div>Uppercase: {uppercase}</div>;
+  };
+  textAtom;
 
   return (
     <div className="App">
-      hello
+      <Input />
+      <CharCount />
+      <Uppercase />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
