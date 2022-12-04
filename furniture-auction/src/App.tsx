41c416f3fc1cfbf97fd192674efeea3f233b5c07
kyubhin <kyubhinhan@gmail.com>
2022-12-04 15:12:17 +0900

    Feat : jotai, eslint & prettier
    jotai, eslint 및 prettier 적용함

--- furniture-auction/src/App.tsx
+++ furniture-auction/src/App.tsx
@@ -1,32 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { atom, useAtom } from 'jotai'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const textAtom = atom('hello')
+const textLenAtom = atom((get) => get(textAtom).length)
+const uppercaseAtom = atom((get) => get(textAtom).toUpperCase())
+
+const Input = () => {
+  const [text, setText] = useAtom(textAtom)
+  return <input value={text} onChange={(e) => setText(e.target.value)} />
+}
+
+const CharCount = () => {
+  const [len] = useAtom(textLenAtom)
+  return <div>Length: {len}</div>
+}
+
+const Uppercase = () => {
+  const [uppercase] = useAtom(uppercaseAtom)
+  return <div>Uppercase: {uppercase}</div>
+}
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      hello
     </div>
   )
 }
