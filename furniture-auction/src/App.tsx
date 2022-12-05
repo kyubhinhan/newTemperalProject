ad0756472b7294b61a137058fda8bfdce59d8ca9
kyubhin <kyubhinhan@gmail.com>
2022-12-05 09:32:06 +0900

    Feat : routing 처리 해줌
    기본적인 routing 처리 해줌

--- furniture-auction/src/App.tsx
+++ furniture-auction/src/App.tsx
@@ -1,34 +1,18 @@
-import { atom, useAtom } from 'jotai';
-import { getOriginalNode } from 'typescript';
-import './App.css';
+import { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const LayoutPage = lazy(() => import('./pages/LayoutPage'));
+const MainPage = lazy(() => import('./pages/MainPage'));
 
 function App() {
-  const textAtom = atom('hello');
-  const textLenAtom = atom((get) => get(textAtom).length);
-  const uppercaseAtom = atom((get) => get(textAtom).toUpperCase());
-
-  const Input = () => {
-    const [text, setText] = useAtom(textAtom);
-    return <input value={text} onChange={(e) => setText(e.target.value)} />;
-  };
-
-  const CharCount = () => {
-    const [len] = useAtom(textLenAtom);
-    return <div>Length: {len}</div>;
-  };
-
-  const Uppercase = () => {
-    const [uppercase] = useAtom(uppercaseAtom);
-    return <div>Uppercase: {uppercase}</div>;
-  };
-  textAtom;
-
   return (
-    <div className="App">
-      <Input />
-      <CharCount />
-      <Uppercase />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<LayoutPage />}>
+          <Route path="/" element={<MainPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
--- furniture-auction/src/main.tsx
+++ furniture-auction/src/main.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { Provider } from 'jotai'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'jotai';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
-)
+);
